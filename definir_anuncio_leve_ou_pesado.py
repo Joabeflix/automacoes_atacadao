ca706b80ea14be0f9fd74e570c26d94a0ed07636
Joabe Alves <joabealvesyt@gmail.com>
2025-04-09 12:32:53 -0300

    new file:   definir_anuncio_leve_ou_pesado.py new file:   enderecos_estoque.xlsx new file:   enderecos_estoque_pd.xlsx new file:   teste.py

---
+++ definir_anuncio_leve_ou_pesado.py
@@ -0,0 +1,26 @@
+import pandas as pd
+
+
+def estilo_prateleira():
+    faixa_1 = [f"{i:03}" for i in range(1, 33)] 
+    faixa_2 = [f"{j:02}" for j in range(1, 25)]
+    letras = ['A', 'B', 'C', 'D', 'E', 'F', 'G']
+
+    enderecos = [f"{n1}-{n2}-{letra}" for n1 in faixa_1 for n2 in faixa_2 for letra in letras]
+
+
+    df = pd.DataFrame(enderecos, columns=["Endereço"])
+    df.to_excel("enderecos_estoque_teste2.xlsx", index=False)
+
+
+
+def enderecos_pd():
+    faixa_1 = [x for x in range(6)]
+    faixa_2 = [f"{x:02}" for x in range(1, 10)]
+    letras = ['A', 'B', 'C', 'D']
+
+    enderecos = [f"PD{n1}-{n2}-{letra}" for n1 in faixa_1 for n2 in faixa_2 for letra in letras]
+    df = pd.DataFrame(enderecos, columns=["Endereço"])
+    df.to_excel("enderecos_estoque_pd.xlsx", index=False)
+
+enderecos_pd()
