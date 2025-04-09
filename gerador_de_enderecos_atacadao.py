5f1c87cae7b055f486970a670220e05fb00fa6a9
Joabe Alves <joabealvesyt@gmail.com>
2025-04-09 16:54:57 -0300

    renamed:    definir_anuncio_leve_ou_pesado.py -> gerador_de_enderecos_atacadao.py

--- gerador_de_enderecos_atacadao.py
+++ gerador_de_enderecos_atacadao.py
@@ -1,6 +1,5 @@
 import pandas as pd
 
-
 def estilo_prateleira():
     faixa_1 = [f"{i:03}" for i in range(1, 33)] 
     faixa_2 = [f"{j:02}" for j in range(1, 25)]
@@ -8,11 +7,8 @@ def estilo_prateleira():
 
     enderecos = [f"{n1}-{n2}-{letra}" for n1 in faixa_1 for n2 in faixa_2 for letra in letras]
 
-
     df = pd.DataFrame(enderecos, columns=["Endereço"])
     df.to_excel("enderecos_estoque_teste2.xlsx", index=False)
-
-
 
 def enderecos_pd():
     faixa_1 = [x for x in range(6)]
@@ -20,7 +16,11 @@ def enderecos_pd():
     letras = ['A', 'B', 'C', 'D']
 
     enderecos = [f"PD{n1}-{n2}-{letra}" for n1 in faixa_1 for n2 in faixa_2 for letra in letras]
+
     df = pd.DataFrame(enderecos, columns=["Endereço"])
     df.to_excel("enderecos_estoque_pd.xlsx", index=False)
+
+
+
 
 enderecos_pd()
