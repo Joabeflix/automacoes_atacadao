1319b8d1581a8f42283c990d3a7c29f357f392e2
Joabe Alves <joabealvesyt@gmail.com>
2025-05-09 09:37:58 -0300

    modified:   baixar_imagens/main.py

--- baixar_imagens/main.py
+++ baixar_imagens/main.py
@@ -1,6 +1,7 @@
 import requests
-import os
 import pandas as pd
+
+
 
 def baixar_imagem(url, local_salvar):
     try:
@@ -14,17 +15,23 @@ def baixar_imagem(url, local_salvar):
         print(f"Falha ao baixar a imagem de {url}: {e}")
         return False
 
+
+
 planilha = pd.read_excel(r'baixar_imagens\Links.xlsx')
 lista_links = planilha['link']
 lista_nomes = planilha['nome']
 
-lista_erros = []
 
+
+
+lista_erros = []
 for l, n in zip(lista_links, lista_nomes):
     local_salvar = f'baixar_imagens/imagens/{n}.jpg'
     if baixar_imagem(url=l, local_salvar=local_salvar):
         continue
     lista_erros.append(n)
+
+
 
 if lista_erros:
     print('Erros:')
