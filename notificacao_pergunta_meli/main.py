ea5a44a2935329e99ca6ee6531cc3d84b49d83be
Joabe Alves <joabealvesyt@gmail.com>
2025-05-20 09:33:48 -0300

    modified:   notificacao_pergunta_meli/__pycache__/main.cpython-312.pyc new file:   notificacao_pergunta_meli/caio.mp3 new file:   notificacao_pergunta_meli/joabe.mp3 new file:   notificacao_pergunta_meli/keyton.mp3 modified:   notificacao_pergunta_meli/main.py

--- notificacao_pergunta_meli/main.py
+++ notificacao_pergunta_meli/main.py
@@ -1,8 +1,22 @@
-from selenium import webdriver
+from selenium import webdriver 
 from selenium.webdriver.common.by import By
 import pickle
 import time
+import pygame
+import random
+import pyautogui
 import winsound
+
+
+# Inicializa o mixer de áudio
+pygame.mixer.init()
+
+# Mapeia os áudios com base no número sorteado
+audios = {
+    1: "joabe.mp3",
+    2: "keyton.mp3",
+    3: "caio.mp3"
+}
 
 driver = webdriver.Chrome()
 
@@ -16,6 +30,20 @@ for cookie in cookies:
     driver.add_cookie(cookie)
 
 driver.get("https://www.mercadolivre.com.br/resumo#menu-user")
+
+# Aguarda a página carregar
+time.sleep(5)
+
+# Obtém tamanho da tela (resolução)
+largura, altura = pyautogui.size()
+
+# Define a posição no canto direito, meio vertical
+x = largura - 100     # 10px da borda direita
+y = altura // 2      # Meio da altura
+
+# Move o mouse e clica
+pyautogui.click(x, y)
+
 time.sleep(5)
 
 while True:
@@ -38,6 +66,27 @@ while True:
         print("Erro ao capturar o texto:", str(e))
 
     if 'Não há perguntas a responder.' not in lista_dados:
-        winsound.Beep(500, 5000)
 
-    time.sleep(300)
+        winsound.Beep(500, 100)
+        winsound.Beep(1000, 50)
+        winsound.Beep(500, 100)
+        winsound.Beep(1000, 50)
+        winsound.Beep(500, 100)
+        winsound.Beep(1000, 50)
+        winsound.Beep(500, 100)
+        winsound.Beep(1000, 50)
+        winsound.Beep(500, 100)
+        winsound.Beep(1000, 50)
+
+        numero = random.randint(1, 3)
+        audio_sorteado = audios[numero]
+        print(f"[INFO] Nova pergunta detectada — tocando áudio {numero}: {audio_sorteado}")
+        
+        pygame.mixer.music.load(audio_sorteado)
+        pygame.mixer.music.play()
+
+        # Espera o áudio terminar
+        while pygame.mixer.music.get_busy():
+            time.sleep(1)
+
+    time.sleep(200)
