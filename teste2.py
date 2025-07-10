68a4ac4f66815d28488a2183411a3d0ec4045f97
Joabe Alves <joabealvesyt@gmail.com>
2025-07-10 14:30:37 -0300

    new file:   imagens_verificar.txt

--- teste2.py
+++ teste2.py
@@ -1,2 +1,7 @@
+teste = ['alves', 'joabe', 'luz']
 
 
+with open('teste.txt', 'r') as file:
+    for x in file:
+        _x = x.strip()
+        print(f'Sim - {_x}' if _x in teste else f'Não - {_x}')
--- verificar_se_foto_ok.py
+++ verificar_se_foto_ok.py
@@ -1,32 +1,27 @@
-import pandas as pd
-import os
 from tkinter import filedialog
+import os
 
-planilha = pd.read_excel('plan.xlsx')
-coluna_codigos = planilha['mpn']
-
-coluna_nova = []
+caminho_txt_ = rf'{os.getcwd()}\imagens_verificar.txt'
 
 print('Selecione a pasta das imagens')
+
 os.chdir(filedialog.askdirectory())
 
 fotos = os.listdir()
-
 lista_nao = []
 
-for codigo in coluna_codigos:
-    codigo = f'{codigo}.jpg'
-    if codigo in fotos:
-        coluna_nova.append('OK')
-        continue
-    coluna_nova.append('Não')
-    lista_nao.append(codigo)
+with open(caminho_txt_, 'r') as file:
+    for codigo in file:
+        _codigo = codigo.strip()
+        _codigo = f'{_codigo}.jpg'
 
-planilha['Foto'] = coluna_nova
-salvar = planilha.to_excel('verif.xlsx', index=False)
-print('Planilha salva com os dados.')
+        if _codigo not in fotos:
+            lista_nao.append(codigo)
 
 if lista_nao:
     print('\nLista de produtos que não está com imagens:')
-    for x in lista_nao:
-        print(x)
+    for codigo in lista_nao:
+        print(codigo)
+
+if not lista_nao:
+    print('Todas as imagens está OK')
